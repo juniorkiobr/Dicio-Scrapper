30bb05c04dda1c0d0edce1b405c8e1b2dbc26b6f
juniorkiobr <juniorkiolouco66@gmail.com>
2023-04-17 08:12:47 -0300

    implementando o link para a

--- api/index.js
+++ api/index.js
@@ -2,22 +2,34 @@ const app = require('express')();
 // import { v4 as uuidv4 } from 'uuid';
 // const { v4: uuidv4 } = require('uuid');
 
-app.get('/api', (req, res) => {
-    const path = `/api/word/banana`;
+// app.get('/api', (req, res) => {
+//     const path = `/api/word/banana`;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
+
+
+// app.get('/api/item/:slug', (req, res) => {
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug}`);
+// });
+
+// app.get('/api/word/:palavra', (req, res) => {
+//     const { palavra } = req.params;
+//     res.end(`Palavra: ${palavra}`);
+// });
+
+app.get('/', (req, res) => {
+    res.send('You are in the root of the API, go to /word/:word to get the word');
+});
+
+app.get('/word/:word', async (req, res) => {
+    const word = req.params.word;
+    const data = await scrapper.getWord(word);
     res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
-
-app.get('/api/word/:palavra', (req, res) => {
-    const { palavra } = req.params;
-    res.end(`Palavra: ${palavra}`);
+    res.setHeader('Cache-Control', 's-max-age=9000, stale-while-revalidate');
+    res.send(data);
 });
 
 module.exports = app;
