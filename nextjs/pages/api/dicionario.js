831e99bef2727a64786fe593238597a9b6cf68da
juniorkiobr <juniorkiolouco66@gmail.com>
2023-04-17 22:21:08 -0300

    Consertando espaçamento do autor das frases.

--- nextjs/pages/api/dicionario.js
+++ nextjs/pages/api/dicionario.js
@@ -1,6 +1,3 @@
-// import edgeChromium from 'chrome-aws-lambda';
-// import puppeteer from 'puppeteer-core';
-
 import fetch from 'node-fetch';
 const { JSDOM } = require('jsdom');
 
@@ -10,11 +7,6 @@ const LOCAL_MACHINE_CHROME_PATH = "C:\\Program Files\\Google\\Chrome\\Applicatio
 
 
 async function asyncFetch(url) {
-    // const page = await browser.newPage();
-    // await page.goto(url);
-    // await page.setViewport({ width: 1080, height: 1024 });
-    // const html = await page.;
-    // await browser.close();
     const htmlText = await fetch(url);
 
     const html = await dicioParser(await htmlText.text());
@@ -29,27 +21,6 @@ async function asyncGetProp(element, attr) {
     } catch (error) {
         // console.log(error);
         return null;
-    }
-}
-
-async function asyncReturnJson(element) {
-    // o jsonValue é para remover do handler e ficar somente o text
-    console.log(element);
-
-    if (element instanceof Promise) {
-        return await (await element).jsonValue();
-    }
-    return await element.jsonValue();
-
-}
-
-async function jsonToArr(json) {
-    if (json != null && json != 'null') {
-        return Object.keys(json).map((key) => {
-            return json[Number(key)];
-        });
-    } else {
-        return [];
     }
 }
 
@@ -92,13 +63,13 @@ function replaceTags(str, string = "") {
 //<em>  -- proverbios?
 
 async function dicioParser(htmlText) {
-    // console.log(htmlText)
     const dom = new JSDOM(htmlText)
     const page = dom.window.document;
     let palavra = await querySelector(page, "div.title-header > h1")
-    // palavra = await asyncReturnJson(asyncGetProp(palavra, "innerHTML")) // usando puppeteer
     palavra = await asyncGetProp(palavra, "innerHTML"); //usando JSDOM
-    palavra = palavra.match(/\s{2,}[A-Za-zÀ-ÖØ-öø-ÿ]+\s{2,}/g)[0].replace(/\s/g, "")
+    let match = palavra.match(/\s{2,}[A-Za-zÀ-ÖØ-öø-ÿ]+\s{2,}/g);
+    if (!match) return { status_code: 400, erro: "Não foi possivel encontar uma página com a palavra desejada" }
+    palavra = match[0].replace(/\s/g, "")
     let significados = [];
     let sinonimos = [];
     let frases = [];
@@ -109,18 +80,13 @@ async function dicioParser(htmlText) {
     if (significadosEl?.length > 0) {
         for (let i = 0; i < significadosEl.length; i++) {
             const element = significadosEl[i];
-            // let innerText = await asyncReturnJson(asyncGetProp(element, "innerHTML")) // usando puppeteer
             let innerText = await asyncGetProp(element, "innerHTML"); // usando JSDOM
-            /*
-            * usado para puppeteer
-            let class_json = await asyncReturnJson(asyncGetProp(element, "classList"))
-            let class_array = await jsonToArr(class_json);
-            */
             let class_Token = await asyncGetProp(element, "classList");
             let class_array = class_Token ? class_Token.value.split(/\s{2,}/g) : [];
-            // console.log(i, class_array)
-            // primeira tentativa de regex pra tags
-            // .replace(/(\s*<\/?[span]+\s*?[a-zA-z=]+(\"?\'?)*[a-zA-z=]*(\"?\'?)*>\s?)+/g, "[split]")
+            /*
+            * primeira tentativa de regex pra tags
+             .replace(/(\s*<\/?[span]+\s*?[a-zA-z=]+(\"?\'?)*[a-zA-z=]*(\"?\'?)*>\s?)+/g, "[split]")
+            */
 
             let innerTextSplit = replaceTags(innerText, "[split]")
                 .split("[split]");
@@ -138,7 +104,6 @@ async function dicioParser(htmlText) {
     if (sinonimosEL?.length > 0) {
         for (let i = 0; i < sinonimosEL.length; i++) {
             const element = sinonimosEL[i];
-            // let innerText = await asyncReturnJson(asyncGetProp(element, "innerHTML")) // usando puppeteer
             let innerText = await asyncGetProp(element, "innerHTML"); // usando JSDOM
 
             sinonimos.push({ palavra: innerText });
@@ -150,7 +115,6 @@ async function dicioParser(htmlText) {
     if (frasesEL?.length > 0) {
         for (let i = 0; i < frasesEL.length; i++) {
             const element = frasesEL[i];
-            // let innerText = await asyncReturnJson(asyncGetProp(element, "innerHTML")) // usando puppeteer
             let innerText = await asyncGetProp(element, "innerHTML"); // usando JSDOM
             let split = innerText
                 .replace(/(\n\s{2,})+/g, "")
@@ -158,11 +122,9 @@ async function dicioParser(htmlText) {
                 .replace("<em>", "[split]")
                 .split("[split]");
 
-            if (split.length > 1) split[1] = replaceTags(split[1]);
+            if (split.length > 1) split[1] = replaceTags(split[1]).replace(/\s{2,}/g, "");
             split[0] = replaceTags(split[0]);
             split[0] = split[0].replace(palavra, "<b>" + palavra + "</b>").replace(/\s{2,}/g, "");
-
-
 
             frases.push({ autor: split[1], frase: split[0] });
 
@@ -171,7 +133,7 @@ async function dicioParser(htmlText) {
 
 
     if (sinonimos.length == 0 && significados.length == 0 && frases.length == 0) {
-        return { status_code: 400, erro: "Palavra não encontrada" }
+        return { status_code: 404, erro: "Palavra não encontrada" }
     } else {
         return { palavra: palavra, significados: significados, sinonimos: sinonimos, frases: frases };
 
@@ -183,20 +145,14 @@ async function dicioParser(htmlText) {
 
 async function getWord(word) {
     try {
-        // const executablePath = await edgeChromium.executablePath || LOCAL_MACHINE_CHROME_PATH;
-        // browser = await puppeteer.launch({
-        // executablePath,
-        // args: edgeChromium.args,
-        // headless: true,
-        // ignoreDefaultArgs: ['--disable-extensions']
-        // })
         const url = url_base + word;
         const response = await asyncFetch(url);
-        // await browser.close();
         return response;
     } catch (error) {
         console.log(error);
-        return { status_code: 400, erro: error }
+        return {
+            status_code: 400, erro: `Ocorreu um erro ao procurar ${word}:. ${error}`
+        }
     };
 
 }
@@ -204,10 +160,10 @@ async function getWord(word) {
 export default async function handler(req, res) {
     const word = req.query.word;
     if (!word) {
-        res.json({ status_code: 400, erro: "Palavra não encontrada" })
+        res.json({ status_code: 404, erro: "Palavra para busca não encontrada" })
     }
     let result = await getWord(word);
-    res.status(200).json(result);
+    res.status(result?.status_code ?? 200).json(result);
 }
 
 
